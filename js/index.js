f4272d5bdde487dd46a8b098be47fb1de680542e
Prudhvi Raj V <99985243+prudv13@users.noreply.github.com>
2022-09-14 22:46:11 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const preloaderWrapper = document.querySelector(".preloader-wrapper");
+
+window.addEventListener("load", function(){
+    preloaderWrapper.classList.add("fade-out-animation");
+    preloaderWrapper.style.display = "none";
+});
